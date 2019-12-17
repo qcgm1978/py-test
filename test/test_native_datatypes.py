b70ff80b221b325bdbb7349e36edd41e39b25caa
qcgm1978 <qcgm197874@gmail.com>
2019-12-17 16:18:09 +0800

    trigonometry

--- test/test_native_datatypes.py
+++ test/test_native_datatypes.py
@@ -1,5 +1,6 @@
 import unittest
 import fractions
+import math
 class TddInPythonExample(unittest.TestCase):
 
     def test_test(self):
@@ -16,8 +17,13 @@ class TddInPythonExample(unittest.TestCase):
         self.assertTrue(callable(fractions.Fraction))
         self.assertFalse(callable(fractions.Fraction(1,3)))
         self.assertFalse(callable(fractions.Fraction(1, 3) * 2))
-        self.assertRaises(ZeroDivisionError, fractions.Fraction,1,0)
+        self.assertRaises(ZeroDivisionError, fractions.Fraction, 1, 0)
 
+    def test_Trigonometry(self):
+        self.assertTrue(math)
+        self.assertAlmostEqual(math.pi,3.14,2)
+        self.assertEqual(math.sin(math.pi/2),1.0)
+        self.assertAlmostEqual(round(math.tan(math.pi/4),2),1.0,2)
    
 
 
