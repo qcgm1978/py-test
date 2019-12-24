d4bccea68c9add4727673c6baa9541b06142de6e
Philipp <philipp@zhanghongliangs-MacBook-Pro.local>
2019-12-24 17:47:46 +0800

    list

--- test/test_native_datatypes.py
+++ test/test_native_datatypes.py
@@ -25,7 +25,40 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(math.sin(math.pi/2),1.0)
         self.assertAlmostEqual(round(math.tan(math.pi/4),2),1.0,2)
    
+    a_list=['a','b','mpilgrim','z','example']
+    def test_list(self):
+        self.assertEqual(self.a_list,['a','b','mpilgrim','z','example'])
+        self.assertEqual(self.a_list[0],'a')
+        self.assertEqual(self.a_list[4],'example')
+        self.assertEqual(self.a_list[-1],'example')
+        self.assertEqual(self.a_list[-3],'mpilgrim')
 
+    def test_slicing(self):
+        self.assertEqual(self.a_list[1:3],['b','mpilgrim'])
+        self.assertEqual(self.a_list[1:-1],['b','mpilgrim','z'])
+        self.assertEqual(self.a_list[0:3],['a','b','mpilgrim'])
+        self.assertEqual(self.a_list[:3],['a','b','mpilgrim'])
+        self.assertEqual(self.a_list[3:],['z','example'])
+        self.assertEqual(self.a_list[:],self.a_list)
+
+    def test_add_items_to_list(self):
+        a_list=['a']
+        self.assertEqual(a_list+[2.0,3],['a',2.0,3])
+        self.assertEqual(a_list.append(True),None)
+        self.assertEqual(a_list,['a',True])
+        self.assertEqual(a_list.extend(['four','Ω']),None)
+        self.assertEqual(a_list,['a',True,'four','Ω'])
+        self.assertEqual(a_list.insert(0,'Ω'),None)
+        self.assertEqual(a_list,['Ω','a',True,'four','Ω'])
+        a_list=['a','b','c']
+        a_list.extend(['d','e','f'])
+        self.assertEqual(a_list,['a','b','c','d','e','f'])
+        self.assertEqual(len(a_list),6)
+        self.assertEqual(a_list[-1],'f')
+        a_list.append(['g','h','i'])
+        self.assertEqual(a_list,['a','b','c','d','e','f',['g','h','i']])
+        self.assertEqual(len(a_list),7)
+        self.assertEqual(a_list[-1],['g','h','i'])
 
 if __name__ == '__main__':
     unittest.main()
