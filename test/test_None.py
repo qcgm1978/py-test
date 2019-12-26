82f2dd14fbcaadbf1cc76b1505ab2a2a505f5cb5
Philipp <philipp@zhanghongliangs-MacBook-Pro.local>
2019-12-26 11:11:56 +0800

    None

--- test/test_None.py
+++ test/test_None.py
@@ -5,9 +5,18 @@ class TddInPythonExample(unittest.TestCase):
 
     def test_none(self):
         self.assertEqual(None, None)
-        self.assertEqual(type(None),None)
+        self.assertIsNone(None)
+        self.assertNotEqual(None,False)
+        self.assertNotEqual(None,0)
+        self.assertNotEqual(None,'')
+        x=None
+        self.assertEqual(None,x)
+        y=None
+        self.assertEqual(y,x)
 
-   
+    def test_none_as_bool(self):
+        self.assertFalse(None)
+        self.assertTrue(not None)
 
 
 
