1f1ffb2e2f97bc516788f3eacb6bf555746bac90
Philipp <philipp@zhanghongliangs-MacBook-Pro.local>
2019-12-26 15:25:20 +0800

    path

--- test/comprehensions/test_current_directory.py
+++ test/comprehensions/test_current_directory.py
@@ -1,5 +1,10 @@
 import os
 import unittest
+import sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from app.humansize import approximate_size  # pylint: disable=import-error
 
 
 class TDDDiveIntoPython3(unittest.TestCase):
@@ -29,7 +34,30 @@ class TDDDiveIntoPython3(unittest.TestCase):
         (shortname,extension)=os.path.splitext(filename)
         self.assertEqual(shortname,'humansize')
         self.assertEqual(extension,'.py')
+    def test_lst_dir(self):
+        os.chdir('/Users/philipp/py-test')
+        self.assertEqual(os.getcwd(),'/Users/philipp/py-test')
+        import glob
+        a_list=glob.glob('examples/*.xml')
+        self.assertEqual(a_list,[])
+        a_list=glob.glob('test/**/test_*.py')
+        self.assertTrue(len(a_list)>0)
+        os.chdir('test/comprehensions')
+        a_list=glob.glob('*test*.py')
+        self.assertTrue(len(a_list)>0)
 
+    def test_metadata(self):
+        self.assertEqual(os.getcwd(),'/Users/philipp/py-test/test/comprehensions')
+        metadata=os.stat('test_current_directory.py')
+        self.assertEqual(type(metadata.st_mtime),float)
+        import time
+        localtime=time.localtime(metadata.st_mtime)
+        self.assertEqual(type(localtime),time.struct_time)
+        self.assertTrue(metadata.st_size>0)
+        size=approximate_size(metadata.st_size)
+        self.assertEqual(type(size),str)
+        real_path=os.path.realpath('test_current_directorh.py')
+        self.assertEqual(real_path,'/Users/philipp/py-test/test/comprehensions/test_current_directorh.py')
 
 if __name__ == '__main__':
     unittest.main()
