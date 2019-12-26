6a0d504bbe7fea8acd5d03400c08794bb391c49d
Philipp <philipp@zhanghongliangs-MacBook-Pro.local>
2019-12-26 10:59:24 +0800

    None

---
+++ test/test_None.py
@@ -0,0 +1,40 @@
+import unittest
+import fractions
+import math
+class TddInPythonExample(unittest.TestCase):
+
+    def test_none(self):
+        self.assertEqual(None, None)
+        self.assertEqual(type(None),None)
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
