ed863c78d65cd28927206d44eaa940603231a87d
张红亮 <zhanghongliang@zhanghongliangs-MacBook-Air.local>
2020-01-01 23:30:57 +0800

    fib

---
+++ test/test_class_iterator/test_iterator.py
@@ -0,0 +1,47 @@
+import unittest
+
+class Fib:
+    '''iterator that yields numbers in the Fibonacci sequence'''
+
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+
+    def __next__(self):
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration
+        self.a= self.b
+        self.b=fib+self.b
+        return fib
+class PapayaWhip:
+    pass
+
+class TDDDiveIntoPython3(unittest.TestCase):
+    def test_class(self):
+        self.assertTrue(Fib)
+        self.assertTrue(type(PapayaWhip))
+        fib=Fib(100)
+        self.assertTrue(type(fib))
+        self.assertTrue(fib.__class__)
+        self.assertEqual(fib.__doc__,'iterator that yields numbers in the Fibonacci sequence')
+        self.assertEqual(fib.max,100)
+        self.assertEqual(Fib(200).max,200)
+        iter(fib)
+        self.assertEqual(fib.a,0)
+        self.assertEqual(fib.b,1)
+        next(fib)
+        self.assertEqual(fib.a,1)
+        self.assertEqual(fib.b,1)
+        arr=[]
+        for n in Fib(10):
+            arr.append(n)
+        self.assertEqual(arr,[0,1,1,2,3,5,8])
+
+
+if __name__ == '__main__':
+    unittest.main()
