15ce01313b5b873ba954f75a9ea83898a14c9c8b
philipp <qcgm197874@gmail.com>
2020-01-08 18:00:52 +0800

    StopIteration

---
+++ test/test_iterators/test_permutations.py
@@ -0,0 +1,31 @@
+import unittest, itertools
+
+class TestPermutation(unittest.TestCase):
+    def test_combinatorics(self):
+        perms=itertools.permutations([1,2,3],2)#	The permutations() function takes a sequence (here a list of three integers) and a number, which is the number of items you want in each smaller group
+        perm_next=next(perms)
+        self.assertEqual(perm_next,(1,2))
+        self.assertEqual(next(perms),(1,3))
+        self.assertEqual(next(perms),(2,1))
+        self.assertEqual(next(perms),(2,3))
+        self.assertEqual(next(perms),(3,1))
+        self.assertEqual(next(perms),(3,2))
+        self.assertRaises(StopIteration,lambda: next(perms))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
