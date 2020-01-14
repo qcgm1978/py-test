2d681a8f266d4e3ee1d2ef11379ba163d8870240
philipp <qcgm197874@gmail.com>
2020-01-14 14:23:48 +0800

    js

---
+++ __test__/js.spec.js
@@ -0,0 +1,17 @@
+it(`list(array) slice`, () => {
+  let a_list = ["a", 2.0, 3, true];
+  a_list.push("four", "Ω");
+  expect(a_list).toEqual(["a", 2.0, 3, true, "four", "Ω"]);
+  a_list.unshift("Ω");
+  expect(a_list).toEqual(["Ω", "a", 2.0, 3, true, "four", "Ω"]);
+});
+it(`push`, () => {
+  let a_list = ["a", "b", "c"];
+  a_list.push("d", "e", "f");
+  expect(a_list).toEqual(["a", "b", "c", "d", "e", "f"]);
+  a_list = ["a", "b", "c"];
+  a_list.push(["d", "e", "f"]);
+  expect(a_list).toEqual(["a", "b", "c", ["d", "e", "f"]]);
+  expect(a_list.length).toBe(4);
+  expect(a_list.slice(-1)[0]).toEqual(["d", "e", "f"]);
+});
---
+++ test/native_datatypes/test_list.py
@@ -0,0 +1,33 @@
+import unittest
+
+class test_list(unittest.TestCase):
+    def test_slicing(self):
+        a_list=['a','b','mpilgrim','z','example']
+        self.assertEqual(a_list[1:3],['b','mpilgrim'])
+        self.assertEqual(a_list[1:-1],['b','mpilgrim','z'])
+        self.assertEqual(a_list[0:3],['a','b','mpilgrim'])
+        self.assertEqual(a_list[:3],['a','b','mpilgrim'])
+        self.assertEqual(a_list[3:],['z','example'])
+        self.assertEqual(a_list[:],a_list)
+
+    def test_add_item(self):
+        a_list=['a']
+        a_list+=[2.0,3]
+        self.assertEqual(a_list,['a',2.0,3])
+        a_list.append(True)
+        self.assertEqual(a_list,['a',2.0,3,True])
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
