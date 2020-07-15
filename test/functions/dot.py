8c11ea795816bfd0f42753acb8709c2a8e408b94
Philipp <zhanghongliang@192.168.1.103>
2020-07-16 00:28:54 +0800

    matrix multiplication

--- test/functions/dot.py
+++ test/functions/dot.py
@@ -1,20 +1,41 @@
 # Dot product of two arrays. Specifically,
+import os
 import unittest
-from dotTwo import dotTwo 
-import numpy as np
+import sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+sys.path.append("/Users/zhanghongliang/Documents/py-test")
+
+from dotTwo import dotTwo
+import numpy as np  # pylint: disable=import-error
+
+print(sys.version)
+
+
 class NumpyTest(unittest.TestCase):
     def setUp(self):
         def assertEqualAll(arr):
-            return all(v==arr[0] for v in arr)
-        self.assertEqualAll=assertEqualAll
-    def test_dot(self):
-        dot=np.dot(3, 4)
-        bool=self.assertEqualAll([dot,3*4,dotTwo(3,4),12])
-        self.assertTrue(bool)
-        dot=np.dot([2j, 3j], [2j, 3j])
-        dot1=dotTwo([2j, 3j], [2j, 3j])
-        bool=self.assertEqualAll([dot,dot1])
-        self.assertTrue([dot,dot1,-13,-13+0j])
+            return all(v == arr[0] for v in arr)
 
-if __name__ == '__main__':
-    unittest.main()        
+        self.assertEqualAll = assertEqualAll
+
+    def test_dot(self):
+        dot = np.dot(3, 4)
+        bool = self.assertEqualAll([dot, 3 * 4, dotTwo(3, 4), 12])
+        self.assertTrue(bool)
+        dot = np.dot([2j, 3j], [2j, 3j])
+        dot1 = dotTwo([2j, 3j], [2j, 3j])
+        bool = self.assertEqualAll([dot, dot1])
+        self.assertTrue([dot, dot1, -13, -13 + 0j])
+        a = [[1, 0], [0, 1]]
+        b = [[4, 1], [2, 2]]
+        c = dotTwo(a, b)
+        d = np.dot(a, b)
+        self.assertEqualAll([(c - d).any(),  0])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
--- test/functions/dotTwo.py
+++ test/functions/dotTwo.py
@@ -1,19 +1,36 @@
 # Dot product of two arrays. Specifically
+def isComplexNum(i):
+    num=i[0] if isinstance(i,list) else i
+    return isinstance(num,complex)
 def dotTwo(a,b):
+    isComplex=isComplexNum(a)
     a=getList(a)
     b=getList(b)
     if(type(a)==list):
         total=0
-        for i in range(len(a)):
-            total-=a[i]*b[i]
+        if(isComplex):
+            for i in range(len(a)):
+                total-=a[i]*b[i]
+        else:
+            row = len(a)
+            cols = len(b[0])
+            total = [[0 for x in range(cols)] for x in range(row)] 
+            for i in range(len(a)):
+                lenM=len(a[i])
+                for m in range(lenM):
+                    tot=ind=0
+                    for n in range(cols):
+                        tot+=a[i][n]*b[m][n]
+                    total[m][i]=(tot)
     else:
         total= a*b
     return total
+
 def getList(arr):
     if(type(arr)==list):
         a_list=[]
         for i in arr:
-            if isinstance(i,complex):
+            if isComplexNum(i):
                 m=int(i.imag)
             else:
                 m=i
