a7c6f14f329feee29a8b7821f5eea46bc122b05f
Philipp <zhanghongliang@192.168.1.104>
2020-07-21 17:42:35 +0800

    dir

--- test/library/dict.py
+++ test/library/dict.py
@@ -2,9 +2,12 @@
 import unittest
 class TDDdict(unittest.TestCase):
     def test_dict(self):
-        
-        self.assertTrue(True)
-
+        # Create a new dictionary. The dict object is the dictionary class. 
+        d=dict(a=1, b=2, c=3)
+        self.assertTrue(d)
+        self.assertDictContainsSubset(d,{'a':1,'b':2,'c':3})
+        self.assertDictEqual(d,{'a':1,'b':2,'c':3})
+        self.assertIsInstance(d,dict)
 if __name__ == '__main__':
     unittest.main()
 
--- test/library/dir.py
+++ test/library/dir.py
@@ -2,8 +2,22 @@
 import unittest
 class TDDdir(unittest.TestCase):
     def test_dir(self):
-        
+        # The resulting list is sorted alphabetically. For example:
         self.assertTrue(True)
+        import struct
+        d=dir()   # show the names in the module namespace  
+        self.assertEqual(d,['self','struct'])
+        s=dir(struct)   # show the names in the struct module 
+        self.assertEqual(s,['Struct', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', '_clearcache', 'calcsize', 'error', 'iter_unpack', 'pack', 'pack_into', 'unpack', 'unpack_from'])
+        class Shape:
+            pass
+        s = Shape()
+        self.assertEqual(len( dir(s)),26)
+        def dir1(self):
+                return ['area', 'perimeter', 'location']
+        Shape.__dir__=dir1
+        self.assertEqual(s.__dir__(),['area', 'perimeter', 'location'])
+        self.assertCountEqual(dir(s),['area', 'perimeter', 'location'])
 
 if __name__ == '__main__':
     unittest.main()
