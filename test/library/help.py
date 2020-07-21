105ae016b807a0c2023569f80ae54f7f75331143
Philipp <zhanghongliang@192.168.1.104>
2020-07-21 18:02:16 +0800

    id

--- test/library/help.py
+++ test/library/help.py
@@ -2,9 +2,11 @@
 import unittest
 class TDDhelp(unittest.TestCase):
     def test_help(self):
-        
-        self.assertTrue(True)
-
+    #    Invoke the built-in help system. (This function is intended for interactive use.) If no argument is given, the interactive help system starts on the interpreter console. If the argument is a string, then the string is looked up as the name of a module, function, class, method, keyword, or documentation topic, and a help page is printed on the console. If the argument is any other kind of object, a help page on the object is generated.
+        h=help(help)
+        self.assertIsNone(h)
+        # help()
+        # exit
 if __name__ == '__main__':
     unittest.main()
 
--- test/library/id.py
+++ test/library/id.py
@@ -2,8 +2,16 @@
 import unittest
 class TDDid(unittest.TestCase):
     def test_id(self):
-        
-        self.assertTrue(True)
+        # Return the “identity” of an object. This is an integer which is guaranteed to be unique and constant for this object during its lifetime. Two objects with non-overlapping lifetimes may have the same id() value.
+        self.assertTrue(callable(id))
+        i = {}
+        iId=id(i)
+        self.assertIsInstance(iId, int)
+        m = {}
+        self.assertNotEqual(iId, id(m))
+        i = None
+        n={}
+        self.assertEqual(iId, id(n))
 
 if __name__ == '__main__':
     unittest.main()
