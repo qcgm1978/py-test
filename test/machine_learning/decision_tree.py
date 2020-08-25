aa70e24283c62c78c02836175609bfc694bd8831
Philipp <zhanghongliang@192.168.1.101>
2020-08-25 11:53:13 +0800

    gini

--- test/machine_learning/decision_tree.py
+++ test/machine_learning/decision_tree.py
@@ -4,9 +4,17 @@ import unittest, pandas
 
 
 class TDD_DECISION_TREE(unittest.TestCase):
+    def setUp(self):
+        file = "test/machine_learning/shows.csv"
+        d1 = {"UK": 0, "USA": 1, "N": 2}
+        d2 = {"YES": 1, "NO": 0}
+        mapData = {"Nationality": d1, "Go": d2}
+        self.d = DataTypes({"file": file, "mapData": mapData})
+        return super().setUp()
+
     def test_read_data_set(self):
         file = "test/machine_learning/shows.csv"
-        d = DataTypes()
+        d = DataTypes({"file": file})
         df = d.getCsvData(file)
         # print(df)
         data = np.array(df)
@@ -25,50 +33,55 @@ class TDD_DECISION_TREE(unittest.TestCase):
 
     def test_decision_tree(self):
         file = "test/machine_learning/shows.csv"
-        img='test/machine_learning/mydecisiontree.png'
+        img = "test/machine_learning/mydecisiontree.png"
         X = ["Age", "Experience", "Rank", "Nationality"]
         y = "Go"
         d1 = {"UK": 0, "USA": 1, "N": 2}
         d2 = {"YES": 1, "NO": 0}
         dictionary = {"Nationality": d1, "Go": d2}
         d = DataTypes()
-        d\
-            .getAndFormatData(file, dictionary)\
-            .createDecisionTreeData(X, y)\
-            .graphByData(img)\
-            .show()
+        # d\
+        #     .getAndFormatData(file, dictionary)\
+        #     .createDecisionTreeData(X, y)\
+        #     .graphByData(img)\
+        #     .show()
+
     def test_first_step(self):
-        pass
-    def test_gini(self):
-        # Gini = 1 - (x/n)2 - (y/n)2
-        # Where x is the number of positive answers("GO"), n is the number of samples, and y is the number of negative answers ("NO"), which gives us this calculation:
-        file = "test/machine_learning/shows.csv"
-        d = DataTypes()
-        df = d.getCsvData(file)
+        df = self.d.df
         go = df["Go"]
         y = go
         self.assertIsInstance(y, pandas.core.series.Series)
-        df = d.mapStrToNum({"Go": {"YES": 1, "NO": 0}})
-        go = df["Go"]
-        one = go == 1
-        x = go[one].shape[0]
-        n = df.shape[0]
-        y = n - x
-        Gini = 1 - (x / n) ** 2 - (y / n) ** 2
-        self.assertAlmostEqual(Gini, 0.497, 3)
-        self.assertEqual(n,13)
+        Gini,n=self.d.getGini("Go")
         value = [round(n * Gini), round(n * (1 - Gini))]
-        self.assertEqual(value,[6,7])
         zero = go == 0
         dfZero = df[zero]
         rank = dfZero["Rank"]
         lessSpecifiedV = rank <= 6.5
         rank1 = rank[lessSpecifiedV]
         self.assertEqual(rank1.shape[0], 5)
-        goCounts=dfZero[dfZero['Go']==1]
-        self.assertEqual(goCounts.shape[0],0)
+        goCounts = dfZero[dfZero["Go"] == 1]
+        self.assertEqual(goCounts.shape[0], 0)
+        self.assertAlmostEqual(Gini, 0.497, 3)
+        self.assertEqual(n, 13)
+        self.assertEqual(value, [6, 7])
         # print(dfZero)
 
+    def test_second_true_step(self):
+        df = self.d.df
+        go = df["Go"] == 0
+        rank = df["Rank"]
+        go1 = df[go]
+        goRank = go1[go1["Rank"] <= 6.5]
+        samples = goRank.shape[0]
+        zero = goRank[goRank['Go']==0].shape[0]
+        value = [zero, samples - zero]
+        gini=samples-value[0]
+        self.assertEqual(gini,0)
+        self.assertEqual(samples, 5)
+        self.assertEqual(value,[5,0])
+    def test_second_false_step(self):
+        Gini,n=self.d.getGini("Nationality",x = 7,n = 8)
+        self.assertAlmostEqual(Gini,.219,3)
 
 if __name__ == "__main__":
     unittest.main()
