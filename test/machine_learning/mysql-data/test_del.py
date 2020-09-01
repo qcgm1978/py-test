574c55d60dd14548614e0f987154e2e0bdd8c241
Philipp <zhanghongliang@192.168.1.108>
2020-09-01 12:31:09 +0800

    ditto

--- test/machine_learning/mysql-data/test_del.py
+++ test/machine_learning/mysql-data/test_del.py
@@ -3,12 +3,13 @@ import unittest
 class TDD_DEL(unittest.TestCase):
     def setUp(self):
         self.m = MysqlOp('customers')
-        sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-        val = ("John", "Mountain 21")
-        self.m.insertInto(sql,val)
+        val = [("John", "Mountain 21"),('Joe',"Yellow Garden 2")]
+        self.m.executemany(val)
         return super().setUp()
     def test_del(self):
         ret=self.m.delete({'address' : 'Mountain 21'})
         self.assertEqual(ret,1 )
+        ret1=self.m.delete({'address' : "Yellow Garden 2"})
+        self.assertEqual(ret1,1 )
 if __name__ == '__main__':
     unittest.main()
